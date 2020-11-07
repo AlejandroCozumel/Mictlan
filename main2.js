52a842985714a19af81749f58ac1aaee07f7f2b3
AlejandroCozumel <68302372+AlejandroCozumel@users.noreply.github.com>
2020-11-07 17:21:29 -0600

    Add files via upload

--- main2.js
+++ main2.js
@@ -1,3 +1,21 @@
+//audio
+var llorona = document.querySelector(".llorona").addEventListener("click", function (){
+    var audio = new Audio("audio/llorona.mp3");
+    audio.play();
+    });
+var felipe = document.querySelector(".felipe").addEventListener("click", function (){
+    var audio = new Audio("audio/felipe.mp3");
+    audio.play();
+    });
+var felipe = document.querySelector(".animas").addEventListener("click", function (){
+    var audio = new Audio("audio/animas.mp3");
+    audio.play();
+    });
+var xochitl = document.querySelector(".xochitl").addEventListener("click", function (){
+    var audio = new Audio("audio/xochitl.mp3");
+    audio.play();
+    });
+
 //Select element function
 const selectElement = function (element) {
     return document.querySelector(element);
